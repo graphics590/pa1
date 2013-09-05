728e8236cf4822a1e369996d58ee5be31ef0f75f
Matt Corallo <git@bluematt.me>
2013-09-04 20:22:50 -0400

    Add mike's test-case to the file.

--- test.cpp
+++ test.cpp
@@ -7,6 +7,46 @@
 #include "GBitmap.h"
 #include "GColor.h"
 
+static void inline original_test_case() {
+	// Mike's test-case
+    const int W = 100;
+    const int H = 100;
+    GContext* ctx = GContext::Create(W, H);
+    if (!ctx) {
+        fprintf(stderr, "GContext::Create failed\n");
+        return -1;
+    }
+
+    const GColor color = { 1, 1, 0, 0 };
+    const GPixel pixel = 0xFFFF0000;
+
+    ctx->clear(color);
+
+    GBitmap bitmap;
+    ctx->getBitmap(&bitmap);
+
+    if (W != bitmap.fWidth || H != bitmap.fHeight) {
+        fprintf(stderr, "unexpected width/height [%d %d]\n", bitmap.fWidth, bitmap.fHeight);
+        return -1;
+    }
+
+    for (int y = 0; y < W; ++y) {
+        const char* row = (const char*)bitmap.fPixels + bitmap.fRowBytes * y;
+        for (int x = 0; x < H; ++x) {
+            const GPixel value = ((const GPixel*)row)[x];
+            if (pixel != value) {
+                fprintf(stderr, "at (%d, %d) expected %x but got %x\n",
+                        x, y, pixel, value);
+                return -1;
+            }
+        }
+    }
+
+    delete ctx;
+    fprintf(stderr, "passed.\n");
+    return 0;
+}
+
 static void inline assert_pixels(GContext* ctx, GPixel pixel) {
 	GBitmap bm;
 	ctx->getBitmap(&bm);
@@ -36,6 +76,8 @@ static void inline test_ctx(GContext* ctx, GPixel pixel, GColor color) {
 }
 
 int main(int argc, char** argv) {
+	original_test_case();
+
     GPixel* storage = new GPixel[10000 * 10000];
     GBitmap bitmap;
     bitmap.fWidth = 100;
